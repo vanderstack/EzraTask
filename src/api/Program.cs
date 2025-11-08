f2f121ee1e52d88a141c9f6f6f2a907051dae09c
vanderstack <github@vanderstack.com>
2025-11-07 19:41:47 -0700

    feat(api): Implement GET /health endpoint

--- src/api/Program.cs
+++ src/api/Program.cs
@@ -6,6 +6,7 @@ using System.Threading.Tasks;
 var builder = WebApplication.CreateBuilder(args);
 var app = builder.Build();
 
+app.MapGet("/health", () => Results.Ok(new { Status = "OK" }));
 app.Run();
 
 public partial class Program { }
